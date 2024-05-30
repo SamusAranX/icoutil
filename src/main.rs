1667f9b6f12786af81e40af219afc2971dcfcd25
Emma Alyx Wunder <hallo@emmalyx.dev>
2024-05-30 21:30:52 +0200

    Added PNG extraction feature

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // icnsutil documentation: https://developer.apple.com/library/archive/documentation/GraphicsAnimation/Conceptual/HighResolutionOSX/Optimizing/Optimizing.html
 
+use std::fs::create_dir_all;
 use std::io::ErrorKind;
 use std::path::{Path, PathBuf};
 use clap::Parser;
@@ -33,14 +34,14 @@ struct Args {
 	input: PathBuf,
 }
 
-fn create_ico<P: AsRef<Path>>(input_dir: P, output_file: P) -> Result<(), String> {
-	// supported icon sizes
-	// source: https://learn.microsoft.com/en-us/windows/apps/design/style/iconography/app-icon-construction#icon-scaling
-	let icon_sizes = vec![16, 20, 24, 30, 32, 36, 40, 48, 60, 64, 72, 80, 96, 256];
+// supported icon sizes
+// source: https://learn.microsoft.com/en-us/windows/apps/design/style/iconography/app-icon-construction#icon-scaling
+static ICON_SIZES: &[u32] = &[16, 20, 24, 30, 32, 36, 40, 48, 60, 64, 72, 80, 96, 256];
 
+fn create_ico<P: AsRef<Path>>(input_dir: P, output_file: P) -> Result<(), String> {
 	let mut icon_dir = ico::IconDir::new(ico::ResourceType::Icon);
 
-	for icon_size in icon_sizes {
+	for icon_size in ICON_SIZES {
 		let size_png = format!("{icon_size}.png");
 		let png_file = match std::fs::File::open(input_dir.as_ref().join(&size_png)) {
 			Ok(result) => result,
@@ -48,7 +49,7 @@ fn create_ico<P: AsRef<Path>>(input_dir: P, output_file: P) -> Result<(), String
 				ErrorKind::NotFound => {
 					continue // ignore this one
 				},
-				other_error => {
+				_ => {
 					println!("Can't open {size_png}: {error}");
 					continue
 				}
@@ -56,7 +57,7 @@ fn create_ico<P: AsRef<Path>>(input_dir: P, output_file: P) -> Result<(), String
 		};
 
 		let image = ico::IconImage::read_png(png_file).unwrap();
-		if (image.width(), image.height()) != (icon_size, icon_size) {
+		if (image.width(), image.height()) != (*icon_size, *icon_size) {
 			println!("{size_png} must be {icon_size}×{icon_size} px, but is {}×{} px instead.", image.width(), image.height());
 			continue
 		}
@@ -78,7 +79,37 @@ fn create_ico<P: AsRef<Path>>(input_dir: P, output_file: P) -> Result<(), String
 }
 
 fn extract_pngs<P: AsRef<Path>>(input_file: P, output_dir: P) -> Result<(), String> {
-	unimplemented!()
+	let file = std::fs::File::open(input_file).expect("Can't open input .ico file");
+	let icon_dir = ico::IconDir::read(file).expect("Input file is invalid");
+
+	create_dir_all(&output_dir).expect("Can't create output folder");
+
+	for entry in icon_dir.entries() {
+		let image = entry.decode().expect("Can't decode image");
+
+		let mut file_name = format!("{}", entry.width());
+		if entry.width() != entry.height() || !ICON_SIZES.contains(&entry.width()) {
+			file_name.push_str(format!("x{}", entry.height()).as_str())
+		}
+		if entry.bits_per_pixel() != 32 {
+			file_name.push_str(format!("@{}", entry.bits_per_pixel()).as_str());
+		}
+		file_name.push_str(".png");
+
+		let png_file = match std::fs::File::create(output_dir.as_ref().join(&file_name)) {
+			Ok(result) => result,
+			Err(error) => match error.kind() {
+				_ => {
+					println!("Can't create {file_name}: {error}");
+					continue
+				}
+			},
+		};
+		image.write_png(png_file).expect("Can't write PNG file");
+		println!("Extracted {file_name}");
+	}
+
+	Ok(())
 }
 
 fn main() -> Result<(), String> {
@@ -106,7 +137,12 @@ fn main() -> Result<(), String> {
 	println!("output: {:?}", output_path);
 
 	if output_path.exists() && !args.overwrite {
-		return Err("The output file already exists. Specify --overwrite to overwrite it.".parse().unwrap());
+		if output_path.is_file() {
+			return Err("The output file already exists. Specify --overwrite to overwrite it.".parse().unwrap());
+		} else if output_path.is_dir() {
+			return Err("The output folder already exists. Specify --overwrite to overwrite it.".parse().unwrap());
+		}
+
 	}
 
 	match args.convert {
